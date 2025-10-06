20df66cfbde1f45eae05691e284bcf7767f5ea25
Ayaz2135 <168137282+Ayaz2135@users.noreply.github.com>
2025-10-07 02:36:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import logging
 import sqlite3
 import asyncio
+import imghdr_py as imghdr
 import os
 from datetime import datetime, timedelta
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
